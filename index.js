129694dd7c808a0ca00348948a3c93bf1679bc8b
MutaiNewton56 <mutainewton56.gmail.com>
2024-04-25 15:38:17 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+// index.js
+
+const input = document.getElementById('button');
+
+// Define a function to handle the event
+function clickAlert() {
+    alert('I was clicked!');
+    
 }
+
+
+// Bind the event listener using addEventListener
+input.addEventListener('click', clickAlert);
